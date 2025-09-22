f7567d4c722d85e81117b4e4aeb81e4c2234a936
ArturKust <ppc.kustov@gmail.com>
2025-09-22 14:35:58 +0500

    Update index.php
    Исправил название функции is_numeric()
    в строке 14 добавил равенство ==0

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
-<?php
+  <?php
 $number1 = trim(fgets(STDIN));
-if (!isnumeric($number1)) {
-fwrite(STDERR, "Введите, пожалуйста, число" . PHPEOL);
-exit;
+if (!is_numeric($number1)) {
+    fwrite(STDERR, "Введите, пожалуйста, число" . PHP_EOL);
+    exit(1);
 }
 
 $number2 = trim(fgets(STDIN));
-if (!isnumeric($number2)) {
-fwrite(STDERR, "Введите, пожалуйста, число" . PHPEOL);
-exit;
+if (!is_numeric($number2)) {
+    fwrite(STDERR, "Введите, пожалуйста, число" . PHP_EOL);
+    exit(1);
 }
 
-if ($number2 == ) {
-fwrite(STDERR, "Делить на нельзя" . PHPEOL);
-exit;
+if ($number2 == 0) {
+    fwrite(STDERR, "Делить на ноль нельзя" . PHP_EOL);
+    exit(1);
 }
 
 $number = $number1 / $number2;
-echo $number . PHPEOL;
+echo $number . PHP_EOL;
 ?>
